8edf5e8d826c20bc553772aae48faef085e245b4
Julian Knocke <Julian.Knocke@xing.com>
2012-04-18 13:55:42 +0200

    rename integration test to get recognized by maven

--- src/test/java/org/jlange/proxy/test/IntegrationTest.java
+++ src/test/java/org/jlange/proxy/test/IntegrationTest.java
@@ -1,8 +1,8 @@
 package org.jlange.proxy.test;
 
-import java.net.MalformedURLException;
+import static org.junit.Assert.assertTrue;
 
-import junit.framework.TestCase;
+import java.net.MalformedURLException;
 
 import org.jboss.netty.handler.codec.http.DefaultHttpRequest;
 import org.jboss.netty.handler.codec.http.HttpMethod;
@@ -10,38 +10,40 @@ import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.jboss.netty.handler.codec.http.HttpVersion;
 import org.jlange.proxy.http.Server;
 import org.jlange.proxy.test.client.HttpRequestClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class Integration extends TestCase {
+public class IntegrationTest {
 
     Integer port = 8080;
     Server  proxy;
 
-    @Override
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
 
         proxy = new Server(port);
         proxy.start();
-
-        super.setUp();
     }
 
-    @Override
-    protected void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
 
         proxy.stop();
-
-        super.tearDown();
     }
 
     // TODO make real tests... this ones always succeeds
+    @Test
     public void testChunkedHtml() throws MalformedURLException {
         HttpRequest request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "http://fritz.box:80/index.html");
         request.setHeader("Host", "backend:80");
 
         HttpRequestClient client2 = new HttpRequestClient();
         client2.request(request, "localhost", port);
+        assertTrue(true);
     }
 
+    @Test
     public void testDynamicHtmlWithoutChunks() throws MalformedURLException {
         HttpRequest request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "http://backend:80/mythweb/");
         request.setHeader("Host", "backend:80");
@@ -50,6 +52,7 @@ public class Integration extends TestCase {
         client1.request(request, "localhost", port);
     }
 
+    @Test
     public void testStaticHtml() throws MalformedURLException {
         HttpRequest request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "http://backend:80/index.html");
         request.setHeader("Host", "backend:80");
