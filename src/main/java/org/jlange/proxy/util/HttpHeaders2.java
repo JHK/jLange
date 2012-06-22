a545a3a8c9954ccea927814e887e267bdf3e090c
Julian Knocke <Knocke.Julian@gmail.com>
2012-06-22 14:55:46 +0200

    static class for missing HttpHeaders

---
+++ src/main/java/org/jlange/proxy/util/HttpHeaders2.java
@@ -0,0 +1,14 @@
+package org.jlange.proxy.util;
+
+public class HttpHeaders2 {
+
+    public static final class Proxy {
+        public final static String CONNECTION = "Proxy-Connection";
+    }
+
+    public static final class SPDY {
+        public final static String STREAM_ID   = "X-SPDY-Stream-ID";
+        public final static String STREAM_PRIO = "X-SPDY-Stream-Priority";
+    }
+
+}
