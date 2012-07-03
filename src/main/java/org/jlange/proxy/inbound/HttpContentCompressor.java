30b34b4d3e9e4defedcaaf7b5a8c9743880605d8
Julian Knocke <Knocke.Julian@gmail.com>
2012-07-03 18:50:16 +0200

    do not compress too big js files, contentcompressor just cuts it

--- src/main/java/org/jlange/proxy/inbound/HttpContentCompressor.java
+++ src/main/java/org/jlange/proxy/inbound/HttpContentCompressor.java
@@ -2,6 +2,7 @@ package org.jlange.proxy.inbound;
 
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
 import org.jboss.netty.handler.codec.http.HttpResponse;
 import org.jlange.proxy.util.HttpHeaders2;
 
@@ -28,15 +29,30 @@ public class HttpContentCompressor extends org.jboss.netty.handler.codec.http.Ht
             final HttpResponse response = (HttpResponse) msg;
 
             // skip certain types
-            if (HttpHeaders2.isJPG(response) || HttpHeaders2.isPNG(response)) {
-                ctx.sendDownstream(e);
-            } else {
+            if (isCompressable(response)) {
                 super.writeRequested(ctx, e);
+            } else {
+                ctx.sendDownstream(e);
             }
 
         } else {
             super.writeRequested(ctx, e);
         }
 
+    }
+
+    private static boolean isCompressable(final HttpResponse response) {
+        // images are already compressed
+        if (HttpHeaders2.isJPG(response))
+            return false;
+        if (HttpHeaders2.isPNG(response))
+            return false;
+
+        // TODO: fix upstream
+        // fix for too long javascript
+        if (HttpHeaders2.isJavascript(response) && HttpHeaders.getContentLength(response) > 64 * 1024)
+            return false;
+
+        return true;
     }
 }
