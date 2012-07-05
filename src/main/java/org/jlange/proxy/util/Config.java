5b9b87eacb535803c647eeb24f22688295de679b
Julian Knocke <Knocke.Julian@gmail.com>
2012-07-05 16:46:03 +0200

    fix tmp dir creation

--- src/main/java/org/jlange/proxy/util/Config.java
+++ src/main/java/org/jlange/proxy/util/Config.java
@@ -24,9 +24,6 @@ public class Config {
     public static final Integer MAX_USED_CONNECTIONS = getConfig().getInteger("org.jlange.outbound.max_used_connections", 12);
     public static final Integer MAX_RESPONSE_SIZE    = getConfig().getInteger("org.jlange.outbound.max_response_size", 2 * 1024 * 1024);
 
-    public static final File    TMP_DIRECTORY        = buildTmpDirectory();
-    public static final Integer COMPRESSION_LEVEL    = getConfig().getInteger("org.jlange.proxy.compression_level", 7);
-
     public static final Boolean HTTP_ENABLED         = getConfig().getBoolean("org.jlange.proxy.http.enabled", true);
     public static final Integer HTTP_PORT            = getConfig().getInteger("org.jlange.proxy.http.port", 8080);
     public static final Integer HTTP_SPEEDUP         = getConfig().getInteger("org.jlange.proxy.http.speedup", 120);
@@ -36,6 +33,9 @@ public class Config {
     public static final Integer SPDY_SPEEDUP         = getConfig().getInteger("org.jlange.proxy.spdy.speedup", 0);
     public static final String  SPDY_KEY_STORE       = getConfig().getString("org.jlange.proxy.spdy.ssl.store");
     public static final String  SPDY_KEY_PASS        = getConfig().getString("org.jlange.proxy.spdy.ssl.key");
+
+    public static final Integer COMPRESSION_LEVEL    = getConfig().getInteger("org.jlange.proxy.compression_level", 7);
+    public static final File    TMP_DIRECTORY        = buildTmpDirectory();
 
     public static Boolean isPluginEnabled(final Class<?> plugin) {
         return getConfig().getBoolean(plugin.getName() + ".enabled", true);
