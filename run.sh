4c9901f0c1c0a1c9de85e93ec31b582cfb544a62
Julian Knocke <Julian.Knocke@xing.com>
2013-04-25 19:09:15 +0200

    add server parameter for running the proxy

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Xbootclasspath/p:npn-boot-8.1.2.v20120308.jar -jar target/jLange-*-jar-with-dependencies.jar
+java -server -Xbootclasspath/p:npn-boot-8.1.2.v20120308.jar -jar target/jLange-*-jar-with-dependencies.jar
